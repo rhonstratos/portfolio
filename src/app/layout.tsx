d708ce5c055f137024a28e8b086785606f7269fe
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-25 18:31:52 +0800

    OpenGraph metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,31 @@
+import { Metadata } from "next";
 import "./globals.css";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import metaImage from "./assets/hoshi.gif";
 
 const font = Plus_Jakarta_Sans({
 	subsets: ["latin"],
 });
 
-export const metadata = {
-	title: "Rhon Stratos - Portfolio",
-	description: "Hi, I'm a full-stack developer",
+const title = "Rhon Stratos - Portfolio";
+const description = "Hi, I'm a full-stack developer";
+const url = "https://rhonstratos-portfolio.web.app/";
+
+export const metadata: Metadata = {
+	title: title,
+	description: description,
+	openGraph: {
+		type: "website",
+		url: url,
+		title: title,
+		description: description,
+		siteName: title,
+		images: [
+			{
+				url: metaImage.src,
+			},
+		],
+	},
 };
 
 export default function RootLayout({
