236f067681d9c65beea32a3f5fc7adc1c4dd55d6
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-25 19:26:38 +0800

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,45 @@
-import { Metadata } from "next";
-import "./globals.css";
-import { Plus_Jakarta_Sans } from "next/font/google";
-import metaImage from "./assets/hoshi.gif";
+import { Metadata } from 'next';
+import './globals.css';
+import { Plus_Jakarta_Sans } from 'next/font/google';
+import metaImage from './assets/hoshi.gif';
 
 const font = Plus_Jakarta_Sans({
-	subsets: ["latin"],
+	subsets: ['latin'],
 });
 
-const title = "Rhon Stratos - Portfolio";
+const title = 'Rhon Stratos - Portfolio';
 const description = "Hi, I'm a full-stack developer";
-const url = "https://rhonstratos-portfolio.web.app/";
+const url = 'https://rhonstratos-portfolio.web.app/';
 
 export const metadata: Metadata = {
+	metadataBase: new URL(url),
+	viewport: {
+		width: 'device-width',
+		initialScale: 1,
+		maximumScale: 1,
+	},
 	title: title,
 	description: description,
+	category: 'portfolio',
 	openGraph: {
-		type: "website",
-		url: url,
 		title: title,
 		description: description,
+		url: url,
 		siteName: title,
 		images: [
 			{
 				url: metaImage.src,
+				width: 500,
+				height: 500,
 			},
 		],
+		locale: 'en_US',
+		type: 'website',
 	},
+	themeColor: [
+		{ media: '(prefers-color-scheme: light)', color: '#bde4a8ff' },
+		{ media: '(prefers-color-scheme: dark)', color: '#9cc69bff' },
+	],
 };
 
 export default function RootLayout({
